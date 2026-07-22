72772efab800eb51588bbf6e07c4c370f58efa2c
Someone-let <ereklevepkhva@gmail.com>
2026-07-22 17:48:38 +0400

    sudashda

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,14 +1,14 @@
-import 'dotenv/config';
+// import 'dotenv/config';
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
+// console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
-import { defineConfig } from 'drizzle-kit';
+// import { defineConfig } from 'drizzle-kit';
 
-export default defineConfig({
-  out: './server/drizzle',
-  schema: './server/schema.ts',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
-});
+// export default defineConfig({
+//   out: './server/drizzle',
+//   schema: './server/schema.ts',
+//   dialect: 'postgresql',
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL!,
+//   },
+// });
